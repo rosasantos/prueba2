4a30fec03455fc1f08412073910a79b2e755e2b3
rosasantos <33054200+rosasantos@users.noreply.github.com>
2017-10-26 13:51:26 +0200

    Update HolaMundo.java
    Añadiendo otro saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,6 +6,8 @@
 public class HolaMundo {
 	
 	public static void main (String args[]) {
-          System.out.println("HOLA----");		
+          System.out.println("HOLA----");
+		 System.out.println("Hola Mundo");
+		
 	}
 }
